c71d3a1bdf2f69f621e42cde3651954c32942b2c
kamimura <baipaa0723@gmail.com>
2018-03-06 11:51:29 +0900

    日付の差分計算処理の訂正

--- dateManager.ts
+++ dateManager.ts
@@ -2,8 +2,7 @@ import {ITodo} from "./todoManager";
 
 export function filtering(todo:ITodo,days:number):boolean{
     if(todo.completed == false) return false;
-    
-    const diff = (todo.completedDate - todo.genDate);
 
-    return todo.completedDate.getTime() - todo.genDate.setDate(todo.genDate.getTime() + days) > 0;
+    const diff = (todo.completedDate.getDate() - todo.genDate.getDate());
+    return (diff/(1000*60*60*24)) > 3;
 }
