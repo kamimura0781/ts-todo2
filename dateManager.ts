61107d06844a9e857521967e7ed441ce27a2c924
kamimura <baipaa0723@gmail.com>
2018-03-07 09:56:12 +0900

    ファイル階層の変更テスト

--- dateManager.ts
+++
@@ -1,9 +0,0 @@
-import {ITodo} from "./todoManager";
-
-export function filtering(todo:ITodo,days:number):boolean{
-    if(todo.completed == false) return false;
-
-    const diff = new Date().getTime() - todo.completedDate!.getTime();
-    const daysDiff = diff/(1000*60*60*24);
-    return daysDiff > days;
-}
--- idManager.ts
+++
@@ -1,8 +0,0 @@
-import {readFile,writeFile} from "./io";
-
-export function genId(){
-    const counter:number = parseInt(readFile('counter.txt'))
-    const newCounter=counter+1;
-    writeFile('counter.txt',newCounter);
-    return newCounter;
-}
